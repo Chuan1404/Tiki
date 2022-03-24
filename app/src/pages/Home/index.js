7a5e438bdbd3ec8587f517517541836547a4c08c
An chu <chuan14402@gmail.com>
2022-03-24 10:30:24 +0700

    net

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,14 +1,38 @@
 import { MenuSide } from 'components';
 import React from 'react';
+import Flickity from 'react-flickity-component';
 import './style.scss';
 
 export default function Home() {
-
+    const flickityOptions = {
+        initialIndex: 0,
+        prevNextButtons: true
+        // contain: true,
+        // setGallerySize: false
+        // accessibility: true
+    }
     return (
         <div id='home'>
             <div className='container home-container'>
                 <div className='homeSide'>
                     <MenuSide />
+                </div>
+                <div className='homeSection'>
+                    <h1>Điện Tử - Điện Lạnh</h1>
+                    <div className='banner'>
+                        <Flickity
+                            className={'carousel'} // default ''
+                            elementType={'div'} // default 'div'
+                            options={flickityOptions} // takes flickity options {}
+                            disableImagesLoaded={false} // default false
+                            reloadOnUpdate // default false
+                            static // default false
+                        >
+                            <img src="imgs/banner1.png" />
+                            <img src="imgs/banner2.jpg" />
+                            <img src="imgs/banner3.jpg" />
+                        </Flickity>
+                    </div>
                 </div>
             </div>
         </div>
