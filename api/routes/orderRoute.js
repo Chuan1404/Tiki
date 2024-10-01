1b1716f175161126222ab346e9a299328fb7de3b
An Chu <LS071>
2024-10-01 13:07:59 +0700

    order

--- api/routes/orderRoute.js
+++ api/routes/orderRoute.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const orderController = require("../controllers/OrderController")
+const authToken = require("../utils/authToken")
 
+router.use(authToken)
 router.get("/", orderController.getAll)
 router.post("/add", orderController.add)
 
