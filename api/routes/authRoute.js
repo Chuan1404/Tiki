f9566d2f70820d7322a24673b41c513e60974073
AnChu <80581001+Chuan1404@users.noreply.github.com>
2024-11-23 06:09:08 +0700

    setup api

--- api/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const authController = require("../controllers/AuthController")
-
-router.post("/login", authController.login)
-router.post("/register", authController.register)
-router.post("/refresh-token", authController.refreshToken)
-
-module.exports = router
