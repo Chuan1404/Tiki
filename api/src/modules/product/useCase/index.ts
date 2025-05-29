68e92d1bf3c9f16a8eea37d18a6bbb53f1e55604
AnChu <80581001+Chuan1404@users.noreply.github.com>
2025-05-29 14:25:41 +0700

    format

--- api/src/modules/product/useCase/index.ts
+++ api/src/modules/product/useCase/index.ts
@@ -1,103 +1,103 @@
 import { v7 } from "uuid";
 import { EModelStatus } from "../../../share/model/enums";
 import {
-  ErrDataExisted,
-  ErrDataInvalid,
-  ErrDataNotFound,
+    ErrDataExisted,
+    ErrDataInvalid,
+    ErrDataNotFound,
 } from "../../../share/model/errors";
 import { PagingDTO } from "../../../share/model/paging";
 import { IProductRepository, IProductUseCase } from "../interface";
 import { Product, ProductSchema } from "../model";
 import {
-  ProductCondDTO,
-  ProductCreateDTO,
-  ProductCreateSchema,
-  ProductUpdateDTO,
-  ProductUpdateSchema,
+    ProductCondDTO,
+    ProductCreateDTO,
+    ProductCreateSchema,
+    ProductUpdateDTO,
+    ProductUpdateSchema,
 } from "../model/dto";
 
 export class ProductUseCase implements IProductUseCase {
-  constructor(private readonly repository: IProductRepository) {}
+    constructor(private readonly repository: IProductRepository) {}
 
-  async create(data: ProductCreateDTO): Promise<string> {
-    const {
-      success,
-      data: parsedData,
-      error,
-    } = ProductCreateSchema.safeParse(data);
+    async create(data: ProductCreateDTO): Promise<string> {
+        const {
+            success,
+            data: parsedData,
+            error,
+        } = ProductCreateSchema.safeParse(data);
 
-    if (!success) {
-      throw new Error(error.message);
+        if (!success) {
+            throw new Error(error.message);
+        }
+
+        const isExisted = await this.repository.findByCond({
+            name: parsedData.name,
+        });
+
+        if (isExisted) {
+            throw ErrDataExisted;
+        }
+
+        let newId = v7();
+        const Product: Product = {
+            id: newId,
+            name: parsedData.name,
+            brandId: parsedData.brandId,
+            categoryId: parsedData.categoryId,
+            price: parsedData.price,
+            thumbnailUrl: parsedData.thumbnailUrl ?? "",
+            status: EModelStatus.ACTIVE,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        };
+
+        await this.repository.insert(Product);
+
+        return newId;
     }
 
-    const isExisted = await this.repository.findByCond({
-      name: parsedData.name,
-    });
+    async update(id: string, data: ProductUpdateDTO): Promise<boolean> {
+        const {
+            success,
+            data: parsedData,
+            error,
+        } = ProductUpdateSchema.safeParse(data);
 
-    if (isExisted) {
-      throw ErrDataExisted;
+        if (!success) {
+            throw ErrDataInvalid;
+        }
+
+        let Product = await this.repository.get(id);
+
+        if (!Product || Product.status === EModelStatus.DELETED) {
+            throw ErrDataInvalid;
+        }
+
+        return await this.repository.update(id, parsedData);
     }
 
-    let newId = v7();
-    const Product: Product = {
-      id: newId,
-      name: parsedData.name,
-      brandId: parsedData.brandId,
-      categoryId: parsedData.categoryId,
-      price: parsedData.price,
-      thumbnailUrl: parsedData.thumbnailUrl ?? "",
-      status: EModelStatus.ACTIVE,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    async get(id: string): Promise<Product | null> {
+        let data = await this.repository.get(id);
 
-    await this.repository.insert(Product);
+        if (!data || data.status === EModelStatus.DELETED) {
+            throw ErrDataNotFound;
+        }
 
-    return newId;
-  }
-  async update(id: string, data: ProductUpdateDTO): Promise<boolean> {
-    const {
-      success,
-      data: parsedData,
-      error,
-    } = ProductUpdateSchema.safeParse(data);
+        return ProductSchema.parse(data);
+    }
+    
+    async list(cond: ProductCondDTO, paging?: PagingDTO): Promise<Product[]> {
+        let data = await this.repository.list(cond, paging);
 
-    if (!success) {
-      throw ErrDataInvalid;
+        return data ? data.map((item) => ProductSchema.parse(item)) : [];
     }
 
-    let Product = await this.repository.get(id);
+    async delete(id: string, isHard: boolean = false): Promise<boolean> {
+        let Product = await this.repository.get(id);
+        if (!Product || Product.status === EModelStatus.DELETED) {
+            throw ErrDataNotFound;
+        }
 
-    if (!Product || Product.status === EModelStatus.DELETED) {
-      throw ErrDataInvalid;
+        return await this.repository.delete(id, isHard);
     }
-
-    return await this.repository.update(id, parsedData);
-  }
-  async get(id: string): Promise<Product | null> {
-    let data = await this.repository.get(id);
-
-    if (!data || data.status === EModelStatus.DELETED) {
-      throw ErrDataNotFound;
-    }
-
-    return ProductSchema.parse(data);
-  }
-  async list(
-    cond: ProductCondDTO,
-    paging?: PagingDTO
-  ): Promise<Product[]> {
-    let data = await this.repository.list(cond, paging);
-
-    return data ? data.map((item) => ProductSchema.parse(item)) : [];
-  }
-
-  async delete(id: string, isHard: boolean = false): Promise<boolean> {
-    let Product = await this.repository.get(id);
-    if (!Product || Product.status === EModelStatus.DELETED) {
-      throw ErrDataNotFound;
-    }
-
-    return await this.repository.delete(id, isHard);
-  }
 }
